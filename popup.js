b68bbfc7434f75be4d601dd540dbfc73448839ac
Prajwal Chander <123977646+PrajwalChander@users.noreply.github.com>
2024-03-10 12:46:22 +0530

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,5 +1,15 @@
 // popup.js
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button element
+    var checkButton = document.getElementById("checkButton");
+
+    // Add a click event listener to the button
+    checkButton.addEventListener("click", function () {
+        submitVideo();
+    });
+});
+
 function submitVideo() {
     document.getElementById("result").innerText = "Please wait while we process your video...";
 
@@ -7,19 +17,19 @@ function submitVideo() {
     var videoLink = document.getElementById("videoLink").value;
 
     // Send the video link to the Flask backend using fetch
-    fetch('/predict', {
+    fetch('http://127.0.0.1:5000/predict', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ video_link: videoLink })
     })
-    .then(response => response.json())
-    .then(data => {
-        // Update the result div with the prediction result
-        document.getElementById("result").innerText = "Prediction: " + data.prediction;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            // Update the result div with the prediction result
+            document.getElementById("result").innerText = "Prediction: " + data.prediction;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
